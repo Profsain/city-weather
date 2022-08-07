25e69d1b006692061479e2c46bbea809f41931d8
Profsain <profsaincrown@gmail.com>
2022-08-07 07:38:27 +0100

    update headline

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './styles/App.css';
 const App = () => (
   <div className="App">
     <h1>Welcome to City Weather</h1>
+    <h4>Find out the weather condition in your city.</h4>
     <CitiesList />
   </div>
 );
